82a822ae40331d0ba80b415dcb2f4f81cd232569
trudeau <morgan.trudeau@here.com>
2018-03-26 18:38:40 -0700

    added pages

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
   plugins: [
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
-      { from: './app/index.html', to: "index.html" }
+      { from: './app/index.html', to: "index.html" },
+      { from: './app/myauctions.html', to: "myauctions.html" },
+      { from: './app/create.html', to: "create.html" },
+      { from: './app/diamond.png', to: "diamond.png" }
+
     ])
   ],
   module: {
