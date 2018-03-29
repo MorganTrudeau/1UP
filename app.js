1fef20896dc4ee32a222b5e89c4f326ace0dfbcd
trudeau <morgan.trudeau@here.com>
2018-03-28 17:52:10 -0700

    heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'app')))
+  .use(express.static(path.join(__dirname, 'build')))
   .set('build', path.join(__dirname, 'build'))
   .get('/', (req, res) => res.render('app'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
