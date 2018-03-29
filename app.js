494b01d417dc5f0f2be9698fe7f3741f5d2d6485
trudeau <morgan.trudeau@here.com>
2018-03-28 17:17:14 -0700

    procfile

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'app')))
   .set('build', path.join(__dirname, 'build'))
-  .get('/', (req, res) => res.render('index'))
+  .get('/', (req, res) => res.render('app.js'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
